3f26bd67ac1bd2c936e0aa4605f66b468622e17c
hugomers <hugomo0897@gmail.com>
2024-05-23 18:54:50 -0600

    se modifica para que se puedan ver los comparativos y generar pedidos automaticos con un clic

---
+++ dist/pwa/assets/QSpace.61ca0356.js
@@ -0,0 +1,1 @@
+import{B as a,F as e}from"./index.2b3d8d3a.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- dist/pwa/assets/QSpace.96e22552.js
+++
@@ -1,1 +0,0 @@
-import{B as a,F as e}from"./index.f89a5d2a.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
